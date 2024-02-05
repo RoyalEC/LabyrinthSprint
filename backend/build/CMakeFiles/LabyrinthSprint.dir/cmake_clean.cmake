c1e7fe3f604a2b966afd19f7a447f4fe815f6522
Ernie Crews <141796937+RoyalEC@users.noreply.github.com>
2024-02-04 23:19:45 -0500

    Websockets Server Started

---
+++ backend/build/CMakeFiles/LabyrinthSprint.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LabyrinthSprint.dir/main.cpp.o"
+  "CMakeFiles/LabyrinthSprint.dir/main.cpp.o.d"
+  "LabyrinthSprint"
+  "LabyrinthSprint.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LabyrinthSprint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backend/build/CMakeFiles/LabyrinthSprint.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LabyrinthSprint.
+# This may be replaced when dependencies are built.
